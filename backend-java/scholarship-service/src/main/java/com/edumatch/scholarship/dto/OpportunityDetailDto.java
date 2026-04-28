6678dff05e0d309986441f3fccab743708b87469
Thanh Quy <quyht2908@gmail.com>
2026-04-28 22:00:34 +0700

    Add JUnit Test for ScholarshipPullicController.java

--- backend-java/scholarship-service/src/main/java/com/edumatch/scholarship/dto/OpportunityDetailDto.java
+++ backend-java/scholarship-service/src/main/java/com/edumatch/scholarship/dto/OpportunityDetailDto.java
@@ -1,10 +1,14 @@
 package com.edumatch.scholarship.dto;
+
 import lombok.Data;
 
 @Data
 public class OpportunityDetailDto {
     private OpportunityDto opportunity;
     private Float matchScore;
+
+    public OpportunityDetailDto() {
+    }
 
     public OpportunityDetailDto(OpportunityDto opportunity) {
         this.opportunity = opportunity;
